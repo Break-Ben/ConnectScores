aaefce247d15cab9998a036888feb9fe1898068a
Break <80194912+Ben-Break@users.noreply.github.com>
2022-04-29 15:54:52 +0800

    Create Table.js

---
+++ Table.js
@@ -0,0 +1,15 @@
+//Table 1.0
+const Tests = []
+Highcharts.charts.forEach(e => {
+    const scores = e.xAxis[0].series[0].data[0].options
+    Tests.push({
+        Score: e.xAxis[0].series[1].options.data[0]+"%",
+        Min: scores.low+"%",
+        Q1: scores.q1+"%",
+        Median: scores.median+"%",
+        Q3: scores.q3+"%",
+        Max: scores.high+"%"
+    })
+})
+Tests.sort((a,b) => (a.Score > b.Score) ? 1 : ((b.Score > a.Score) ? -1 : 0))
+console.table(Tests)
