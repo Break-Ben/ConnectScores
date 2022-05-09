c12156371351cfa68688d2675f85884456557613
Break <80194912+Ben-Break@users.noreply.github.com>
2022-05-09 19:06:34 +0800

    Added rounding

--- Tooltip.js
+++ Tooltip.js
@@ -1,14 +1,15 @@
-// Tooltip 1.1
+//Tooltip 1.2
+let round = (num) => Math.round(num*100)/100
 Highcharts.charts.forEach(e => {
     if(e) {
-        var options = e.options
+        options = e.options
         options.tooltip.enabled = true
-        options.chart.style.height = 1000
+        options.chart.style.overflow = 'visible'
         options.tooltip.formatter = function () {
             var tooltip = '<div class="Tooltip">'
             if(this.series.index == 0) {
                 const points = this.point.options
-                tooltip += '<b>Lowest Score:</b> ' + points.low + '<br><b>Lower Quartile:</b> ' + points.q1 + '<br><b>Median:</b> ' + points.median + '<br><b>Upper Quartile:</b> ' + points.q3 + '<br><b>Highest Score:</b> ' + points.high
+                tooltip += '<b>Lowest Score:</b> ' + round(points.low) + '<br><b>Lower Quartile:</b> ' + round(points.q1) + '<br><b>Median:</b> ' + round(points.median) + '<br><b>Upper Quartile:</b> ' + round(points.q3) + '<br><b>Highest Score:</b> ' + round(points.high)
             }
             else {
                 tooltip += '<b>Your Score:</b> ' + this.y
